38d2aaf2094276218055e5d76d916741ba517ac1
Ingo Pratsch <pratingo@mailbox.org>
2021-09-11 10:40:45 +0200

    Implements basic todos

--- index.js
+++ index.js
@@ -1,63 +1,87 @@
 const chalk = require('chalk');
+const { rawListeners, exit } = require('process');
+const randomColor = require('randomcolor');
+const readline = require('readline');
 
-const row = 31;
-const col = 9;
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
-const randomColor = Math.floor(Math.random() * 4);
-
-const colors = ['red', 'green', 'blue', 'yellow'];
-let color = process.argv[2] ? process.argv[2] : colors[randomColor];
-
-let innerText;
-
-switch (process.argv[2]) {
-  case 'red':
-    color = 'red';
-    innerText = '#ff0000';
-    break;
-  case 'green':
-    color = 'green';
-    innerText = '#00ff00';
-    break;
-  case 'blue':
-    color = 'blue';
-    innerText = '#0000ff';
-    break;
-  case 'yellow':
-    color = 'yellow';
-    innerText = '#f0f000';
-    break;
-  default:
-    color = colors[randomColor];
-    innerText = '#RANDOM';
+let done = false;
+function ask() {
+  let colo, lumi;
+  rl.question('Color?', (col) => {
+    rl.question('Luminousity?', (lum) => {
+      randomColor({
+        luminosity: lum,
+        hue: col,
+      });
+      done = true;
+      rl.close();
+    });
+  });
+  if (colo && lumi) {
+    printLogo();
+  }
 }
 
-function topBottom() {
-  for (let i = 0; i < col / 3; i++) {
+if (process.argv[2] === 'ask') {
+  ask();
+}
+
+const color = randomColor({
+  luminosity: process.argv[3],
+  hue: process.argv[2],
+});
+
+function printLogo(row = 21, col = 9) {
+  for (let i = 0; i < col; i++) {
     let str = '';
+    let arr = [];
     for (let j = 0; j < row; j++) {
       str += '#';
     }
-    console.log(chalk[color](str));
-  }
-}
-
-function middle() {
-  for (let i = 0; i < col / 3; i++) {
-    let str = '';
-
-    for (let j = 0; j < row; j++) {
-      if (i === 1) {
-        str = `#####       ${innerText}       #####`;
-        break;
-      } else {
-        str = '#####                     #####';
+    if (
+      i === Math.floor(col / 2 - 1) ||
+      i === Math.floor(col / 2) ||
+      i === Math.floor(col / 2 + 1)
+    ) {
+      str = `#####           #####`;
+      if (i === Math.floor(col / 2)) {
+        str = `#####  ${color}  #####`;
       }
+
+      arr = [...str];
+      const add = Math.floor((row - 21) / 2);
+      for (let k = 0; k < add; k++) {
+        arr.unshift('#');
+        arr.push('#');
+      }
+      str = arr.join('');
     }
-    console.log(chalk[color](str));
+    console.log(chalk.hex(color)(str));
   }
 }
+/*
+if (process.argv[2] === 'ask') {
+  printLogo();
 
-topBottom();
-middle();
-topBottom();
+  rl.close();
+  exit();
+}
+*/
+if (process.argv[2]?.includes('x')) {
+  let str = process.argv[2].split('x');
+
+  printLogo(str[0], str[1]);
+  rl.close();
+  exit();
+}
+if (process.argv[2] === undefined) {
+  printLogo();
+  rl.close();
+} else {
+  printLogo();
+  rl.close();
+}
